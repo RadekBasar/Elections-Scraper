19f324cc3f33d08fc71f690f578749e8905c330a
Radek Basár <radek_basar@kb.cz>
2022-04-18 11:09:59 +0200

    Projekt č. 3: Elections Scraper_úprava formátu II

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ URL_BASE = "https://volby.cz/pls/ps2017nss/"
 
 def scrape_villages(url):
     parsed_page = get_parsed_page(url)
+    village_info_header_items = ["Číslo obce", "Obec"]#get_village_info_header_items(parsed_page)
+    village_details_header_items = None
     villages_info = get_villages_info(parsed_page)
     scrapped_villages = []
     for village_info in villages_info:
@@ -18,6 +20,7 @@ def scrape_villages(url):
             merged_village_to_write = []
             for district_url in district_urls:
                 village_parsed_page = get_parsed_page(district_url)
+                village_details_header_items = get_village_details_header_items(village_parsed_page)
                 village_details = get_village_details(village_parsed_page)
                 village_to_write = village_item_to_write(village_info, village_details)
                 if not merged_village_to_write:
@@ -26,9 +29,12 @@ def scrape_villages(url):
                     merged_village_to_write = merge_village_items(merged_village_to_write, village_to_write)
             scrapped_villages.append(merged_village_to_write)
         else:
+            village_details_header_items = get_village_details_header_items(village_parsed_page)
             village_details = get_village_details(village_parsed_page)
             village_to_write = village_item_to_write(village_info, village_details)
             scrapped_villages.append(village_to_write)
+    header_items = village_item_to_write(village_info_header_items, village_details_header_items)
+    scrapped_villages.insert(0, header_items)
     return scrapped_villages
 
 
@@ -75,6 +81,17 @@ def get_villages_info(parsed_page):
         quit("Get village info error")
 
 
+# Vraci hlavicky pro prvni 2 polozky
+def get_village_info_header_items(parsed_page):
+    try:
+        table = parsed_page.find("div", id="inner").find("table")
+        header_item_code = table.find("th", class_="fixed45", id="t1sb1").text
+        header_item_name = table.find("th", class_="fixed150", id="t1sb2").text
+        return header_item_code, header_item_name
+    except:
+        quit("Get village header info error")
+
+
 # Vraci seznam okrsku v obci - URL
 def get_district_urls(parsed_page):
     try:
@@ -94,13 +111,24 @@ def get_district_urls(parsed_page):
 def get_village_details(parsed_page):
     try:
         table = parsed_page.find("table")
-        voters_count = table.find("td", class_="cislo", headers="sa2").text
-        envelops_count = table.find("td", class_="cislo", headers="sa3").text
-        valid_votes_count = table.find("td", class_="cislo", headers="sa6").text
+        header_item_voters_count = table.find("td", class_="cislo", headers="sa2").text
+        header_item_envelops_count = table.find("td", class_="cislo", headers="sa3").text
+        header_item_valid_votes_count = table.find("td", class_="cislo", headers="sa6").text
         parties = get_parties(parsed_page)
-        return voters_count, envelops_count, valid_votes_count, parties
+        return header_item_voters_count, header_item_envelops_count, header_item_valid_votes_count, parties
     except:
         quit("Get villages detail error")
+
+
+def get_village_details_header_items(parsed_page):
+    try:
+        voters_count = "Voliči v seznamu"
+        envelops_count = "Vydané obálky"
+        valid_votes_count = "Platné hlasy"
+        parties_header_items = get_parties_header_items(parsed_page)
+        return voters_count, envelops_count, valid_votes_count, parties_header_items
+    except:
+        quit("Get villages detail header items error")
 
 
 def get_parties(parsed_page):
@@ -119,6 +147,24 @@ def get_parties(parsed_page):
         return parties
     except:
         quit("Get parties error")
+
+
+def get_parties_header_items(parsed_page):
+    try:
+        parties_header_items_names = []
+        parties_tables = parsed_page.find("div", id="inner").find_all("table")
+        for table_index in range(len(parties_tables)):
+            parties_table = parties_tables[table_index]
+            rows = parties_table.find_all("tr")
+            for row in rows:
+                table_number = table_index + 1
+                headers = "t" + str(table_number) + "sa1 t" + str(table_number) + "sb2"
+                party_header_item_name_tag = row.find("td", class_="overflow_name", headers=headers)
+                if party_header_item_name_tag:
+                    parties_header_items_names.append(party_header_item_name_tag.text)
+        return parties_header_items_names
+    except:
+        quit("Get parties header items error")
 
 
 def village_item_to_write(village_info, village_details):
